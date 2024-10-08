1e834398a7c9fdaa4f9666aab77e6dee61cdf76f
dopper <doppermrj@gmail.com>
2024-10-08 12:53:34 -0600

    Updated dirc.sh to include folder sizes

--- dirc.sh
+++ dirc.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-# Loop through all directories in the current directory
-for dir in */; do
-    # Use find to count files within the directory (including subdirectories)
-    file_count=$(find "$dir" -type f | wc -l)
-    echo "$dir has $file_count files"
+# Loop through all directories (including hidden ones) in the current directory
+for dir in $(find . -maxdepth 1 -type d \
+    -not -path "./Library" \
+    -not -path "./Library/*" \
+    -not -path "./System" \
+    -not -path "./Volumes" \
+    -not -path "./private" \
+    -not -path "./dev" \
+    -not -path "./bin" \
+    -not -path "./sbin" \
+    -not -path "./Pictures/Photos Library.photoslibrary"); do
+    
+    # Use find to count files within the directory (including hidden subdirectories)
+    file_count=$(find "$dir" -type f 2>/dev/null | wc -l)
+    
+    # Use du to calculate the size of the directory
+    dir_size=$(du -sh "$dir" 2>/dev/null | cut -f1)
+    
+    echo "$dir has $file_count files and is $dir_size in size"
 done
 
